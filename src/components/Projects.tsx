117da4d51517be3bec7ea196a3762b00e690fe34
Ian Hansson <ian.hansson@gmail.com>
2020-11-27 05:48:58 +0000

    Add react springs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,19 +2,47 @@ import React, { ReactNode } from "react";
 import { Heading } from "./Elements";
 import { Link } from "react-router-dom";
 import { Block } from "./Grid";
+import { animated, useTransition } from "react-spring";
 
-export const Projects = ({ children }: { children?: ReactNode }) => (
-  <section>
-    <h3 style={{ marginBottom: "1em" }}>
-      You have {React.Children.count(children)} projects.
-    </h3>
-    <Block type="minor" withStyle="is-muted-alt">
-      {children}
-    </Block>
-  </section>
-);
+export const Projects = ({ children }: { children?: ReactNode }) => {
+  const items = [
+    {
+      key: 1,
+      text: "A",
+    },
+    {
+      key: 2,
+      text: "B",
+    },
+    {
+      key: 3,
+      text: "C",
+    },
+  ];
+  const transitions = useTransition(items, (item) => item.key, {
+    from: { opacity: 0, transform: "translate3d(50px,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0,0,0)" },
+    config: { duration: 500 },
+    trail: 300,
+  });
+
+  return (
+    <section>
+      <h3 style={{ marginBottom: "1em" }}>
+        You have {React.Children.count(children)} projects.
+      </h3>
+      <Block type="minor" withStyle="is-muted-alt">
+        {transitions.map(({ props, key }) => (
+          <animated.div key={key} style={{ ...props }} className="is-divided">
+            <Project />
+          </animated.div>
+        ))}
+      </Block>
+    </section>
+  );
+};
 export const Project = () => (
-  <article className="is-divided">
+  <article>
     <Heading type="minor">Project</Heading>
     <Link to="/dashboard/projects/1">View project</Link>
   </article>
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,21 +1,61 @@
 import React, { ReactNode } from "react";
-import { Block, Column, Grid } from "./Grid";
+import { Block, Grid } from "./Grid";
 import { Heading } from "./Elements";
 import { Link } from "react-router-dom";
+import { useSpring, animated, useTransition } from "react-spring";
 
-export const Tasks = ({ children }: { children?: ReactNode }) => (
-  <div>
-    <h3 style={{ marginBottom: "1em" }}>
-      You have {React.Children.count(children)} tasks due.
-    </h3>
-    <Grid>{children}</Grid>
-  </div>
-);
+export const Tasks = ({ children }: { children?: ReactNode }) => {
+  const tasksDueOpacity = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 2500 },
+  });
+
+  const items = [
+    {
+      key: 1,
+      text: "A",
+    },
+    {
+      key: 2,
+      text: "B",
+    },
+    {
+      key: 3,
+      text: "C",
+    },
+  ];
+  const transitions = useTransition(items, (item) => item.key, {
+    from: { opacity: 0, transform: "translate3d(0,40px,0)" },
+    enter: { opacity: 1, transform: "translate3d(0,0px,0)" },
+    trail: 300,
+  });
+
+  return (
+    <div>
+      <h3 style={{ marginBottom: "1em" }}>
+        You have{" "}
+        <animated.span style={tasksDueOpacity}>
+          {React.Children.count(children)}
+        </animated.span>{" "}
+        tasks due.
+      </h3>
+      <Grid>
+        {transitions.map(({ props, key }) => (
+          <animated.div
+            key={key}
+            style={{ ...props, flexGrow: 1, marginRight: "1em" }}
+          >
+            <Task />
+          </animated.div>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 export const Task = () => (
-  <Column withStyle="is-muted-alt" spacing="1">
-    <Block type="minor">
-      <Heading type="minor">Task</Heading>
-      <Link to="/projects">View project</Link>
-    </Block>
-  </Column>
+  <Block type="minor" withStyle="is-muted-alt">
+    <Heading type="minor">Task</Heading>
+    <Link to="/projects">View project</Link>
+  </Block>
 );
